c00764094af2e0bccae76ae38c26b5e8cb1115ca
Hlonela <hloni.mabhani@gmail.com>
2021-05-20 15:52:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Basic Calculator
+
+class Add:
+    def __init__(self):
+        pass
+    def add (self, a, b):
+        return a+b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+    def subtract (self, a, b):
+        return a-b
+
+class Divide:
+    def __init__(self):
+        pass
+    def devide (self, a, b):
+        return a/b
+
+class Multiply:
+    def __init__(self):
+        pass
+    def multiply (self, a, b):
+        return a*b
